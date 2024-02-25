e687437ebb6fadb811168c971bc4c4f1e75b63a4
sulphurninja <sulphurninja@gmail.com>
2024-02-26 03:36:03 +0530

    first commit

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-12'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '59gkqdwv',
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
